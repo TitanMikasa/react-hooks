2e393db9e477fe84e3aced951c8c2dc47340dd83
Kate <test@gmail.com>
2021-01-11 12:37:21 -0500

    excersize 01

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -2,28 +2,36 @@
 // http://localhost:3000/isolated/exercise/01.js
 
 import * as React from 'react'
+import PropTypes from 'prop-types'
 
-function Greeting() {
+function Greeting({initialName = ''}) {
   // 💣 delete this variable declaration and replace it with a React.useState call
-  const name = ''
+  const [name, setName] = React.useState(initialName)
 
   function handleChange(event) {
     // 🐨 update the name here based on event.target.value
+    setName(event.target.value)
+    window.setName = setName
   }
-
+  window.setName = setName
+  console.log('call re-render function');
   return (
     <div>
       <form>
         <label htmlFor="name">Name: </label>
-        <input onChange={handleChange} id="name" />
+        <input onChange={handleChange} id="name" value={name} />
       </form>
       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
     </div>
   )
 }
 
+Greeting.propTypes = {
+  initialName: PropTypes.string,
+}
+
 function App() {
-  return <Greeting />
+  return <Greeting initialName="bob" />
 }
 
 export default App
